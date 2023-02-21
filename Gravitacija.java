fdcb7e1c942a765019be8d6359c6e37f6f67e83a
MrTine <tm13018@student.uni-lj.si>
2023-02-21 19:18:25 +0100

    Dodaj izračun pospeška #2

--- Gravitacija.java
+++ Gravitacija.java
@@ -4,4 +4,19 @@ public class Gravitacija {
         System.out.println("OIS je zakon!");
     }
 
+    public static double nadmorskaVisina(double v) {
+
+        final double G = 6.674e-11;
+        final double MASA_ZEMLJE = 5.972e24;
+        final double POLMER_ZEMLJE = 6.371e6;
+
+        return (G * MASA_ZEMLJE) / kvadrat(POLMER_ZEMLJE + v);
+
+    }
+
+    public static double kvadrat(double a) {
+        return a * a;
+    }
+
 }
+
