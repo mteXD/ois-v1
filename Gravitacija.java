3dc3c23039dfcd0a283658176746543856096556
mteXD <mp04643@student.uni-lj.si>
2023-02-25 11:28:58 +0100

    Izračun in izpis gravitacijskega pospeška na neki nadmorski višini #3 in slika študenta #4

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Gravitacija {
     
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Vnesite nadmorsko visino (km): ");
+        double visina = sc.nextDouble();
+        System.out.printf("Gravitacija na tej visini: %.2f m/s^2%n", nadmorskaVisina(visina));
         System.out.println("OIS je zakon!");
     }
 
@@ -10,7 +17,7 @@ public class Gravitacija {
         final double MASA_ZEMLJE = 5.972e24;
         final double POLMER_ZEMLJE = 6.371e6;
 
-        return (G * MASA_ZEMLJE) / kvadrat(POLMER_ZEMLJE + v);
+        return (G * MASA_ZEMLJE) / kvadrat(POLMER_ZEMLJE + v * 1000);
 
     }
 
